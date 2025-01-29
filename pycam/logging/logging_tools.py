5c15df0a332e879733bdfc9df0f8c944274ff795
Dan Brady <daniel.brady@sheffield.ac.uk>
2025-01-29 14:00:00 +0000

    update creation of root logger to use LoggerManager methods

--- pycam/logging/logging_tools.py
+++ pycam/logging/logging_tools.py
@@ -75,19 +75,12 @@ root_logger = logging.getLogger()
 # If the root logger doesn't have a file handler then add one
 if not any(isinstance(handler, logging.FileHandler) for handler in root_logger.handlers):
     # Start by defining the root log path
-    root_log_path = Path.home() / "pycam_logs" / "root.log"
-    root_log_path.parent.mkdir(parents=True, exist_ok=True)
+    root_log_path = Path.home() / "pycam_logs/root.log"
 
-    # Then setup a file handler and formatter for the root logger
-    root_file_handler = TimedRotatingFileHandler(
-        root_log_path, when = 'D', interval=1, backupCount=5
-    )
-    root_file_formatter = logging.Formatter(
-        '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-        '%Y-%m-%d %H:%M:%S'
-    )
-
-    root_file_handler.setFormatter(root_file_formatter)
-    root_logger.addHandler(root_file_handler)
-
+    # Add the file handler
+    LoggerManager.add_file_handler(root_logger, root_log_path, level = logging.INFO)
+    
+    root_logger.setLevel(logging.INFO)
+    
+    # Log message on creation
     root_logger.info("New session started")
