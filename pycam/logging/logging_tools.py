680e15fea612bebc2eb89e9f7e9633797cd19d72
Dan Brady <daniel.brady@sheffield.ac.uk>
2025-01-29 13:58:25 +0000

    updates to LoggerManager Add docstrings, tweak add_logger method, add add_file_handler method for use with root logger, move remove_stream_handlers function into class

--- pycam/logging/logging_tools.py
+++ pycam/logging/logging_tools.py
@@ -4,31 +4,70 @@ from pathlib import Path
 from logging.handlers import TimedRotatingFileHandler
 
 class LoggerManager:
+    """ Class for manging creation of loggers and creation and deltion of handlers 
+    for PyCam software
+    """
+
     _loggers = {}  # Store created loggers to prevent duplicates
+    _file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', '%Y-%m-%d %H:%M:%S')
 
     @staticmethod
-    def add_logger(name, colour):
+    def add_logger(name, colour = "white", level = logging.INFO):
+        """ Add a named logger.
+
+        :param str name: Name of the logger to create
+        :param str colour: Colour of the text used for logging messages, defaults to white
+        :param int level: Logging level to set for the StreamHandler, defaults to logging.DEBUG
+        :return logger: Reference to logger
+        """
+
         if name not in LoggerManager._loggers:
             logger = logging.getLogger(name)
+            logger.setLevel(logging.DEBUG)
             if not logger.handlers:  # Only configure if no handlers exist
                 # Configure the logger (e.g., set level, add handlers)
                 format_str = f'%(log_color)s%(levelname)-8s%(reset)s%(asctime)s - %({colour})s%(name)s - %(message)s'
                 formatter = colorlog.ColoredFormatter(format_str, '%Y-%m-%d %H:%M:%S')
                 handler = colorlog.StreamHandler() # Output to console
                 handler.setFormatter(formatter)
+                handler.setLevel(level)  # Set the desired logging level
                 logger.addHandler(handler)
-                logger.setLevel(logging.DEBUG)  # Set the desired logging level
             LoggerManager._loggers[name] = logger
         return LoggerManager._loggers[name]
+    
+    @staticmethod
+    def add_file_handler(logger, log_path, level=logging.DEBUG):
+        """ Add FileHandler to an existing logger. If the log path is new then create a new handler,
+        if not then use the existing one stored in the handler dict.
 
-def remove_stream_handler(logger):
-    """ Removes all stream handlers from logger provided.
+        :param logger logger: Existing logger to add FileHandler to
+        :param (str|Path) log_path: Location to write the log file to
+        :param int level: Logging level to set for the FileHandler, defaults to logging.DEBUG
+        """
+        log_path = Path(log_path)
+        try:
+            log_path.parent.mkdir(parents=True, exist_ok=True)
+        except Exception as e:
+            print(f"Error creating log file: {e}")
 
-    :param logger logger: logger object to remove stream handler from
-    """
-    for handler in logger.handlers:
-        if isinstance(handler, logging.StreamHandler):
-            logger.removeHandler(handler)
+        file_handler_key = str(log_path)  # Use log file path as key
+        if file_handler_key not in LoggerManager._file_handlers:
+            file_handler = TimedRotatingFileHandler(log_path, when = 'D', interval=1, backupCount=5)
+            file_handler.setFormatter(LoggerManager._file_formatter)
+            file_handler.setLevel(level)
+            LoggerManager._file_handlers[file_handler_key] = file_handler
+        
+        logger.addHandler(LoggerManager._file_handlers[file_handler_key])
+
+    @staticmethod
+    def remove_stream_handlers(logger):
+        """ Removes all stream handlers from logger provided.
+
+        :param logger logger: logger object to remove stream handlers from
+        """
+        for handler in logger.handlers:
+            if isinstance(handler, logging.StreamHandler):
+                logger.removeHandler(handler)
 
 # Set up the root logger
 root_logger = logging.getLogger()
