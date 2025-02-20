1a4b32483fb98719a4995e18667b2421a687a484
christopher-wild <c.wild@sheffield.ac.uk>
2025-02-20 14:02:05 +0000

    Fix config absolute paths with Pyplis method

--- pycam/scripts/process_doas.py
+++ pycam/scripts/process_doas.py
@@ -2,6 +2,8 @@ import yaml
 from pathlib import Path
 
 from pycam.doas.ifit_worker import IFitWorker
+from pycam.so2_camera_processor import PyplisWorker
+
 
 if __name__ == '__main__':
     args = IFitWorker.get_args()
@@ -14,14 +16,22 @@ if __name__ == '__main__':
                  'Ring': {'path': './pycam/doas/calibration/Ring.txt', 'value': 0.1}
                  }
 
+    # Expand paths
+    ils_path = PyplisWorker.expand_config_path(None, path= config['ILS_path'], config_dir=Path(args.config).parent)
+    ld_lookup_1 = PyplisWorker.expand_config_path(None, path= config['ld_lookup_1'], config_dir=Path(args.config).parent)
+    ld_lookup_2 = PyplisWorker.expand_config_path(None, path= config['ld_lookup_2'], config_dir=Path(args.config).parent)
+    spec_dir = PyplisWorker.expand_config_path(None, path= config['spec_dir'], config_dir=Path(args.config).parent)
+    dark_dir = PyplisWorker.expand_config_path(None, path= config['dark_img_dir'], config_dir=Path(args.config).parent)
+
     # Create ifit object
     ifit_worker = IFitWorker(species=ref_paths, dark_dir=config['dark_img_dir'])
-    ifit_worker.load_ils(config['ILS_path'])  # Load ILS
-    ifit_worker.load_ld_lookup(config['ld_lookup_1'], fit_num=0)
-    ifit_worker.load_ld_lookup(config['ld_lookup_2'], fit_num=1)
+    ifit_worker.load_ils(ils_path)  # Load ILS
+    ifit_worker.load_ld_lookup(ld_lookup_1, fit_num=0)
+    ifit_worker.load_ld_lookup(ld_lookup_2, fit_num=1)
     ifit_worker.corr_light_dilution = 0.0
-    ifit_worker.load_dir(config['spec_dir'], plot=False)  # Load spectra directory
+    ifit_worker.dark_dir = dark_dir
+    ifit_worker.load_dir(spec_dir, plot=False)  # Load spectra directory
     ifit_worker.get_wavelengths(config)
     ifit_worker.get_shift(config)
     # Process directory
-    ifit_worker.start_processing_threadless(Path(config['spec_dir']))
+    ifit_worker.start_processing_threadless(Path(spec_dir))
