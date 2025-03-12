4bc0adc90262f06658116a8b4fe1b5e37c2d6ee1
christopher-wild <c.wild@sheffield.ac.uk>
2025-03-12 18:27:28 +0000

    add roi_abs to opt_flow_settings

--- pycam/scripts/process_image_sequence.py
+++ pycam/scripts/process_image_sequence.py
@@ -6,6 +6,7 @@ from pycam.doas.cfg import doas_worker
 if __name__ == "__main__":
     args = PyplisWorker.get_args()
     pyplis_worker = PyplisWorker(args.config_path)
+    pyplis_worker.reset_self(reset_plot=False)
     pyplis_worker.load_pcs_from_config()
     pyplis_worker.apply_config()
     pyplis_worker.plot_iter = False
@@ -14,6 +15,8 @@ if __name__ == "__main__":
     pyplis_worker.update_cam_geom(pyplis_worker.geom_dict)
     pyplis_worker.measurement_setup(location=pyplis_worker.volcano)
     pyplis_worker.init_results()
+    
+    pyplis_worker.update_opt_flow_settings(roi_abs = pyplis_worker.config['roi_abs'])
     pyplis_worker.img_list = pyplis_worker.get_img_list()
     pyplis_worker.set_processing_directory(img_dir=args.output_directory, make_dir=True)
     pyplis_worker.doas_worker = doas_worker
